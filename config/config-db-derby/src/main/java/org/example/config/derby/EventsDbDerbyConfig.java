aeba23037f3bad489efab4491fbb6ec1f7b90a9f
Vlad Wild <vladwild220@gmail.com>
2021-03-27 10:06:29 +0300

    временный коммит 2

--- config/config-db-derby/src/main/java/org/example/config/derby/EventsDbDerbyConfig.java
+++ config/config-db-derby/src/main/java/org/example/config/derby/EventsDbDerbyConfig.java
@@ -1,13 +1,26 @@
-package org.example.config.h2;
+package org.example.config.derby;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableTransactionManagement
-@EnableJpaRepositories(basePackages = {"org.example.repositories.db.events.derby"})
+@EnableJpaRepositories(basePackages = {"org.example.repositories.db.events.derby"},
+        entityManagerFactoryRef = "derbyEntityManager",
+        transactionManagerRef = "derbyTransactionManager")
 @EntityScan(basePackages = "org.example.model.db.events.derby")
 public class EventsDbDerbyConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix="spring.derby-datasource")
+    public DataSource productDataSource() {
+        return DataSourceBuilder.create().build();
+    }
 }
