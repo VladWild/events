1a3d11441c1049581ec99f99383072b2b8b26db1
Vlad Wild <vladwild220@gmail.com>
2021-04-04 14:57:27 +0300

    временный коммит 2 - вроде как наконц-то настроил 2 базы

--- config/config-db-derby/src/main/java/org/example/config/derby/EventsDbDerbyConfig.java
+++ config/config-db-derby/src/main/java/org/example/config/derby/EventsDbDerbyConfig.java
@@ -1,13 +1,69 @@
-package org.example.config.h2;
+package org.example.config.derby;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableTransactionManagement
-@EnableJpaRepositories(basePackages = {"org.example.repositories.db.events.derby"})
-@EntityScan(basePackages = "org.example.model.db.events.derby")
+@EnableJpaRepositories(basePackages = {"org.example.repositories.db.events.derby"},
+        entityManagerFactoryRef = "entityManagerFactoryDerby",
+        transactionManagerRef = "transactionManagerDerby")
 public class EventsDbDerbyConfig {
+
+    @Bean(name = "dataSourceDerby")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .url("jdbc:derby:target/derbydb;create=true")
+                .driverClassName("org.hibernate.dialect.DerbyDialect")
+                .build();
+    }
+
+    @Bean(name = "entityManagerFactoryDerby")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            EntityManagerFactoryBuilder builder,
+            @Qualifier("dataSourceDerby") DataSource dataSource) {
+        return builder
+                .dataSource(dataSource)
+                .persistenceUnit("events-derby")
+                .packages("org.example.model.db.events.derby")
+                .properties(getHibernateProperties())
+                .build();
+    }
+
+    @Bean(name = "entityManagerDerby")
+    public EntityManager entityManager(
+            @Qualifier("entityManagerFactoryDerby") EntityManagerFactory entityManagerFactory) {
+        return entityManagerFactory.createEntityManager();
+    }
+
+    @Bean(name = "transactionManagerDerby")
+    public PlatformTransactionManager transactionManager(
+            @Qualifier("entityManagerFactoryDerby") EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+
+    private Map<String, String> getHibernateProperties() {
+        Map<String, String> map = new HashMap<>();
+        map.put("hibernate.hbm2ddl.auto", "update");
+        map.put("hibernate.format_sql", "true");
+        map.put("hibernate.show_sql", "true");
+        map.put("hibernate.dialect", "org.hibernate.dialect.DerbyDialect");
+        return map;
+    }
 }
--- config/config-db-h2/src/main/java/org/example/config/h2/EventsDbH2Config.java
+++ config/config-db-h2/src/main/java/org/example/config/h2/EventsDbH2Config.java
@@ -1,13 +1,72 @@
 package org.example.config.h2;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableTransactionManagement
-@EnableJpaRepositories(basePackages = {"org.example.repositories.db.events.h2"})
-@EntityScan(basePackages = "org.example.model.db.events.h2")
+@EnableJpaRepositories(basePackages = {"org.example.repositories.db.events.h2"},
+        entityManagerFactoryRef = "entityManagerFactoryH2",
+        transactionManagerRef = "transactionManagerH2")
 public class EventsDbH2Config {
+
+    @Bean(name = "dataSourceH2")
+    @Primary
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .url("jdbc:h2:mem:test-db-h2")
+                .driverClassName("org.h2.Driver")
+                .build();
+    }
+
+    @Bean(name = "entityManagerFactoryH2")
+    @Primary
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            EntityManagerFactoryBuilder builder,
+            @Qualifier("dataSourceH2") DataSource dataSource) {
+        return builder
+                .dataSource(dataSource)
+                .persistenceUnit("events-h2")
+                .packages("org.example.model.db.events.h2")
+                .properties(getHibernateProperties())
+                .build();
+    }
+
+    @Bean(name = "entityManagerH2")
+    @Primary
+    public EntityManager entityManager(
+            @Qualifier("entityManagerFactoryH2") EntityManagerFactory entityManagerFactory) {
+        return entityManagerFactory.createEntityManager();
+    }
+
+    @Bean(name = "transactionManagerH2")
+    @Primary
+    public PlatformTransactionManager transactionManager(
+            @Qualifier("entityManagerFactoryH2") EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+
+    private Map<String, String> getHibernateProperties() {
+        Map<String, String> map = new HashMap<>();
+        map.put("hibernate.hbm2ddl.auto", "update");
+        map.put("hibernate.format_sql", "true");
+        map.put("hibernate.show_sql", "true");
+        map.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+        return map;
+    }
 }
